24ec98fa14bb0186868faaaa9b54b3aa87906867
Latex2GUD <10484948@ucn.dk>
2023-12-11 15:03:57 +0100

    CustomerCo og OrderCo

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,20 @@
 package controller;
+import java.util.*;
+
+import model.*; 
 
 public class CustomerController {
-
+	private List<Customer> customerList; 
+	
+	public CustomerController() {
+		this.customerList = new ArrayList<>(); 
+	}
+	public Customer findCustomerByPhone(int phone) {
+		for (Customer customer : customerList) {
+			if (customer.getPhone() == phone) {
+				return customer;
+			}
+		}
+		return null;
+	}
 }
--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,7 +1,43 @@
 package controller;
-
-
+import java.util.*;
+import model.*; 
 
 public class OrderController {
-
+	private List<Order> orders; 
+	
+	public OrderController() {
+		this.orders = new ArrayList<>();
+	}
+	public void addOrder(Date orderDate, int orderId) {
+		Order order = new Order(orderDate, orderId); 
+		orders.add(order); 
+	}
+	public List<Order> getAllOrders(){
+		return orders; 
+	}
+	public Order getOrderById(int orderId) {
+		for(Order order : orders) {
+			if(order.getOrderId() == orderId) {
+				return order; 
+			}; 
+		}
+		return null; // hvis ordren ikke bliver fundet
+	}
+	public void updateOrder(int orderId, Date newOrderDate) {
+		Order order = getOrderById(orderId); 
+		if (order != null) {
+			order.setOrderDate(newOrderDate);
+		} else {
+			System.out.println("Ordren blev ikke fundet");
+		}
+	}
+	// Metode, der fjerner en order + 
+	public void removeOrder(int orderId) {
+		Order order = getOrderById(orderId); 
+		if(order != null) {
+			orders.remove(order);
+		} else {
+			System.out.println("Ordren blev ikke fundet"); //Ordren blev ikke fundet ved brug af orderId
+		}
+	}
 }
