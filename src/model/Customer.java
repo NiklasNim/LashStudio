4f6fcf945056513216e6f7720524c8bd028110ab
Latex2GUD <10484948@ucn.dk>
2023-12-11 11:41:13 +0100

    Customer og service setters and getters

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,7 +1,37 @@
 package model;
 
 public class Customer {
-	private String firstName; 
-	private String lastName; 
-	private int phone; 
+	private String firstName;
+	private String lastName;
+	private int phone;
+
+	public Customer(String firstName, String lastName, int phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
--- src/model/Service.java
+++ src/model/Service.java
@@ -1,10 +1,51 @@
 package model;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public class Service {
-	private Timestamp timePeriod; //tjek lige 
-	// private Money price; //tjek lige ud
-	private String description; 
-	private String type; 
+	private Timestamp timePeriod; // tjek lige
+	private BigDecimal price; // tjek lige ud
+	private String description;
+	private String serviceType;
+
+	public Service(Timestamp timePeriod, BigDecimal price, String description, String serviceType) {
+		this.timePeriod = timePeriod;
+		this.price = price;
+		this.description = description;
+		this.serviceType = serviceType;
+	}
+
+	public Timestamp getTimePeriod() {
+		return timePeriod;
+	}
+
+	public void setTimePeriod(Timestamp timePeriod) {
+		this.timePeriod = timePeriod;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+
 }
