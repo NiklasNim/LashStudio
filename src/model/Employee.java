da7d64741798e1861c792d7496b43ee62843c61b
Latex2GUD <10484948@ucn.dk>
2023-12-11 12:10:43 +0100

    Material I Location I Employee - setters and getters

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,6 +1,39 @@
 package model;
 
 public class Employee {
-	private String name;
+	// bookingLine??
+	private String firstName;
+	private String lastName; 
 	private int employeeId; 
+	
+	public Employee(String firstName, String lastName, int employeeId) {
+		this.firstName = firstName; 
+		this.lastName = lastName; 
+		this.employeeId = employeeId; 
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
 }
--- src/model/Material.java
+++ src/model/Material.java
@@ -1,8 +1,48 @@
 package model;
 
 public class Material {
-	private String lashes; 
-	private String nails; 
-	private String haircolour; 
-	private String scissors; 
+	private String lashes;
+	private String nails;
+	private String hairColour;
+	private String scissors;
+
+	public Material(String lashes, String nails, String hairColour, String scissors) {
+		this.lashes = lashes;
+		this.nails = nails;
+		this.hairColour = hairColour;
+		this.scissors = scissors;
+	}
+
+	public String getLashes() {
+		return lashes;
+	}
+
+	public void setLashes(String lashes) {
+		this.lashes = lashes;
+	}
+
+	public String getNails() {
+		return nails;
+	}
+
+	public void setNails(String nails) {
+		this.nails = nails;
+	}
+
+	public String getHairColour() {
+		return hairColour;
+	}
+
+	public void setHairColour(String hairColour) {
+		this.hairColour = hairColour;
+	}
+
+	public String getScissors() {
+		return scissors;
+	}
+
+	public void setScissors(String scissors) {
+		this.scissors = scissors;
+	}
+
 }
