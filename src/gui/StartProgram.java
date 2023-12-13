22bc25447d59517f840bad4753f9d1c8c47797a6
Latex2GUD <10484948@ucn.dk>
2023-12-14 00:26:31 +0100

    RunProgram-klasse opdateret

--- src/gui/StartProgram.java
+++ src/gui/StartProgram.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class StartProgram {
+import javax.swing.SwingUtilities;
 
+public class StartProgram {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
