265b6ffaff5345f1b718c54b01f145323856a3fe
Maltekofod <10484949@ucn.dk>
2023-12-13 16:32:44 +0100

    BookingLine lavet

--- src/model/BookingLine.java
+++ src/model/BookingLine.java
@@ -1,6 +1,37 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class BookingLine {
+	private Service service;
 	private int quantity; 
+	private BigDecimal unitPrice;
 	// private money unitMoney; 
+	
+	public BookingLine(Service service, int quantity, BigDecimal unitPrice) {
+		
+		this.service = service;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	public Service getService() {
+		return service;
+	}
 }
