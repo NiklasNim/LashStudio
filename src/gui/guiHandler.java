7fc93d2c2961cb54d003c9ebb9c2f937fe40b480
NiklasNim <10481979@ucn.dk>
2023-12-17 20:30:06 +0100

    cleanup

--- src/gui/guiHandler.java
+++ src/gui/guiHandler.java
@@ -19,6 +19,9 @@ public class guiHandler  {
     }
 	
 	public static void createWindow(JFrame window) {
+		if (canCreateWindow(window)) {
+			return;
+		}
 		disableCurrent();
 		windows.add(window);
 		window.setVisible(true);
@@ -46,5 +49,9 @@ public class guiHandler  {
 	private static void setSize(JFrame frame) {
 		 frame.setSize(900, 600);
 	     frame.setLocationRelativeTo(null);
+	}
+	
+	private static boolean canCreateWindow(JFrame window) {
+		return windows.size() > 0 && window.getClass().getName() == windows.get(windows.size() - 1).getClass().getName();
 	}
 }
