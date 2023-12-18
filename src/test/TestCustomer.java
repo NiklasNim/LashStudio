89e44a1a1e91b2df5c8160e93c936014c00f9202
Os4ka <10478742@ucn.dk>
2023-12-18 11:42:44 +0100

    Update TestCustomer.java

--- src/test/TestCustomer.java
+++ src/test/TestCustomer.java
@@ -5,36 +5,41 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.Customer;
+import database.CustomerDB;
 
 public class TestCustomer {
 
-	private Customer customer;
+	private CustomerDB customerDB;
+	private Customer existingCustomer;
 
 	@BeforeEach
 	void setUp() {
+		customerDB = new CustomerDB();
+		existingCustomer = customerDB.findCustomerByPhone(20232024);
+
 	}
 
 	@AfterEach
 	void tearDown() {
-		customer = null;
+		customerDB = null;
+		existingCustomer = null;
 	}
 
-	//Testing the customer's first name.
-	@Test 
+	// Testing the customer's first name.
+	@Test
 	void testCustomerFirstName() {
-		assertEquals("Bo", customer.getFirstName(), "The customer's first name should match.");
+		assertEquals("Henrik", existingCustomer.getFirstName(), "The customer's first name should match.");
 	}
 
-	//Testing the customer's last name.
+	// Testing the customer's last name.
 	@Test
 	void testCustomerLastName() {
-		assertEquals("Benzon", customer.getLastName(), "The customer's last name should match.");
+		assertEquals("Larsen", existingCustomer.getLastName(), "The customer's last name should match.");
 	}
 
-	//Testing the customer's phone.
+	// Testing the customer's phone.
 	@Test
 	void testCustomerPhone() {
-		assertEquals("19981999", customer.getPhone(), "The customer phone should match.");
+		assertEquals(20232024, existingCustomer.getPhone(), "	The customer phone should match.");
 	}
-
 }
