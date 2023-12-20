bbd06a03a7f8c98b9ac90001c6d8e3d2e0ace53b
Latex2GUD <10484948@ucn.dk>
2023-12-20 09:55:49 +0100

    Test case 2

--- src/test/TestFindCustomer.java
+++ src/test/TestFindCustomer.java
@@ -25,6 +25,22 @@ public class TestFindCustomer {
 		System.out.println("Testen returnerede kunden: " + foundCustomer.getFirstName() + " "
 				+ foundCustomer.getLastName() + ", hvilket bekræfter testen.");
 	}
+	@Test
+	// Her testes metoden "findCustomerByPhone()" med AAA-Test.
+	void testFindCustomerByPhoneInvalid() {
+
+		// Arrange
+		CustomerController customerController = new CustomerController();
+		int phone = 99999999;
+
+		// Act
+		Customer foundCustomer = customerController.findCustomerByPhone(phone);
+
+		// Assert
+		assertNotNull(foundCustomer, "Kunden burde ikke være null");
+		System.out.println("Testen returnerede kunden: " + foundCustomer.getFirstName() + " "
+				+ foundCustomer.getLastName() + ", hvilket bekræfter testen.");
+	}
 }
 
 
